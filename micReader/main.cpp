d008aa3c3faec8e797b82532d742aada9c1a7a68
Pavel Tyletsky <pashok-new@yandex.ru>
2017-11-05 11:14:15 +0300

    Initial Commit

---
+++ micReader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  micReader
+//
+//  Created by Pavel Tyletsky on 5/11/17.
+//  Copyright © 2017 Pavel Tyletsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
