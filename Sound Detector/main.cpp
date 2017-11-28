0249c5510ca6ea8703f3704cc90c5b3fa63c55a6
Pavel Tyletsky <pashok-new@yandex.ru>
2017-11-28 15:26:11 +0300

    Initial Commit

---
+++ Sound Detector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sound Detector
+//
+//  Created by Pavel Tyletsky on 28/11/17.
+//  Copyright © 2017 Pavel Tyletsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
